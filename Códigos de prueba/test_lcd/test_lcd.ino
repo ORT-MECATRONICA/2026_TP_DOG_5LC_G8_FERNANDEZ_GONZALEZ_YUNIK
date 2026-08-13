c20cda38bb26e5851b5db30cdc40fd6763601a7e
Paulina <49317047@est.ort.edu.ar>
2026-08-13 16:56:11 -0300

    modificacion programa LCD
    Texto modificable por puerto serie

--- Códigos de prueba/test_lcd/test_lcd.ino
+++ Códigos de prueba/test_lcd/test_lcd.ino
@@ -1,28 +1,42 @@
-//ST - TP DOG - Grupo 8 - Santiago Fernández, Paulina Gonzalez y Avner Yunik
-// Código de prueba para LCD i2c
+// ST - TP DOG - Grupo 8 - Santiago Fernández, Paulina Gonzalez y Avner Yunik
+// Código de prueba para LCD i2c con texto cambiante (ingresado por Serial)
+
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// Pinout definition
+// Definición pinout
 #define SCL_PIN 21
 #define SDA_PIN 22
 
 // Configuración del LCD
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+String texto = "Hello World!";
 
 void setup() {
-  // Wire.begin takes (SDA, SCL)
+  // Wire.begin
   Wire.begin(SDA_PIN, SCL_PIN);
+  Serial.begin(115200);
 
   lcd.init();
   lcd.backlight();
 
+  // Primera imrpesión de pantalla
+  lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("ESP32 LCD Test");
-
+  lcd.print("Text Input:");
   lcd.setCursor(0, 1);
-  lcd.print("Status: Working!");
+  lcd.print(texto);
 }
 
 void loop() {
+
+  if (Serial.available() > 0) {
+    texto = Serial.readString();
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Text Input:");
+    lcd.setCursor(0, 1);
+    lcd.print(texto);
+  }
 }
